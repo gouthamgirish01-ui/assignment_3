e85d9e46d820082a9b670fb07b45829e0ac0d6bc
gouthamgirish01-ui <gouthamgirish01@gmail.com>
2026-01-19 16:36:23 +0530

    Update 3_Task-1.py

--- 3_Task-1.py
+++ 3_Task-1.py
@@ -1,8 +1,15 @@
 def factorial(num):
     fact=1
-    for i in range (1,(num+1)):
-        fact=fact*i
-    return fact 
+    if (num>0):
+        for i in range (1,(num+1)):
+            fact=fact*i
+        return fact
+    elif (num==0):
+        return 1
+    else:
+        print("invalid input")
+        return -1
 num = int(input("Enter your number: "))
 fact1=factorial(num)
-print(f"Factorial of {num} is :{fact1}")
+if (fact1>0):
+    print("Factorial of ",num," is:",fact1)
